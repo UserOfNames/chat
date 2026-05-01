1d4bd06b64f779e8a8834dced3a08dcc59511df3
UserOfNames <lightninguy.yoyo@gmail.com>
2026-04-30 22:15:45 -0400

    Refactored config initializer

--- crates/chat_server/src/init/config.rs
+++ crates/chat_server/src/init/config.rs
@@ -1,12 +1,12 @@
-use std::fs::{File, create_dir_all};
-use std::io::{self, Write};
 use std::path::PathBuf;
 
-use anyhow::{Context, bail};
+use anyhow::Context;
 use clap::Args;
 use serde::{Deserialize, Serialize};
 
 use crate::{CONFIG_FILE_NAME, DEFAULT_CONFIG, first_match, utils::get_project_dirs};
+
+use super::{WriteParams, write_with_params};
 
 #[derive(Debug, Args, Serialize, Deserialize)]
 pub struct InitConfigArgs {
@@ -30,47 +30,22 @@ pub fn init_config(args: InitConfigArgs) -> anyhow::Result<()> {
     let config_path = first_match! {
         Some(path) = args.path => path,
         Some(pd) = &project_dirs => pd.config_dir().join(CONFIG_FILE_NAME),
-    };
-
-    let Some(config_path) = config_path else {
-        bail!("Config path could not be resolved");
-    };
+    }
+    .context("Resolving config file path")?;
 
     if args.dry_run {
         println!("{}", config_path.display());
         return Ok(());
     }
 
-    if let Some(parent) = config_path.parent() {
-        create_dir_all(parent)
-            .with_context(|| format!("Creating output directory: '{}'", parent.display()))?;
-    }
+    let paramses = &[WriteParams {
+        path: &config_path,
+        contents: DEFAULT_CONFIG.to_owned(),
+        force: args.force,
+        mode: None,
+    }];
 
-    let file = if args.force {
-        File::create(&config_path)
-    } else {
-        File::create_new(&config_path)
-    };
-
-    let mut file = match file {
-        Ok(file) => file,
-
-        // This error can only arise if File::create_new was used, so there's no need to check
-        // args.force here.
-        Err(e) if e.kind() == io::ErrorKind::AlreadyExists => bail!(
-            "File already exists at path '{}'. Use --force to overwrite it.",
-            config_path.display()
-        ),
-
-        Err(e) => {
-            return Err(e).with_context(|| {
-                format!("Initializing config at path '{}'", config_path.display())
-            });
-        }
-    };
-
-    file.write_all(DEFAULT_CONFIG.as_bytes())
-        .context("Writing default config file")?;
+    write_with_params(paramses).context("Saving config file")?;
 
     println!("Default config initialized at '{}'", config_path.display());
 
